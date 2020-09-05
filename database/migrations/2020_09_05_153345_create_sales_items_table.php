b51546b5f88053c8a37278401c082705a17b02be
OmarFaruk20 <faruk952219@gmail.com>
2020-09-05 13:00:21 -0500

    Created Database Migrations

---
+++ database/migrations/2020_09_05_153345_create_sales_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id');
+            $table->foreignId('sales_invoices_id');
+            $table->double('quantity');
+            $table->double('price');
+            $table->double('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales_items');
+    }
+}
